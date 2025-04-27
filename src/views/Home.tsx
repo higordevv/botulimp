34f83a7842921560803f15cdc4a2852f985cf113
higordevv <pedrohigor2004@gmail.com>
2025-04-26 22:38:50 -0300

    Init About

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,12 +2,14 @@ import Navbar from '../components/Navbar';
 import Header from '../components/Header';
 import ServicesSection from '../components/Services/ServiceSection';
 import Footer from '../components/Footer';
+import About from '../components/About/About';
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-[#c7e9f4] text-[#0d3d66] font-sans">
       <Navbar />
       <Header />
+      <About />
       <ServicesSection />
       <Footer />
     </div>
